b649899e9ef464489c093e4667465f1d26c8d21f
Ryotakemuraryo <rtakemura@ucsd.edu>
2025-05-04 19:30:21 -0700

    change colors

--- global.js
+++ global.js
@@ -28,7 +28,13 @@ const y = d3.scaleLinear()
   .nice()
   .range([height, 0]);
 
-const color = d3.scaleOrdinal(d3.schemeTableau10);
+  const myColors = [
+    "#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd",
+    "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf",
+    "#393b79", "#637939", "#8c6d31", "#843c39", "#7b4173"
+  ];
+  
+  const color = d3.scaleOrdinal(myColors);
 
 // 軸
 g.append("g")
