762272dae0b8a9e2f76e26b51ded1a86104d2a14
Ryotakemuraryo <rtakemura@ucsd.edu>
2025-05-04 19:35:32 -0700

    debug

--- global.js
+++ global.js
@@ -18,7 +18,7 @@ const data = await d3.csv("Mouse_Data_Long.csv", d => ({
 }));
 
 const fullmice = d3.group(data, d => d.mouse);
-const allMice = Array.from(fullGroups.keys());
+const allMice = Array.from(fullmice.keys());
 const selectedMice = allMice.slice(0, Math.floor(allMice.length / 2));
 const groups = new Map(
     selectedMice.map(mouse => [mouse, fullGroups.get(mouse)])
